57ca7d0d582a6da247771767939d3e88b15f094c
Vedant13-ux <56965523+Vedant13-ux@users.noreply.github.com>
2021-02-27 20:22:22 +0530

    auth

--- front-end/src/containers/Landing/RegisterStudents.js
+++ front-end/src/containers/Landing/RegisterStudents.js
@@ -8,106 +8,138 @@ import {
   Button,
 } from "@material-ui/core";
 import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
-import Checkbox from "@material-ui/core/Checkbox";
 import Select from "@material-ui/core/Select";
-import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
+import Alert from '@material-ui/lab/Alert';
+import { apiCallAuth } from '../../api/api'
 
-// const useStyles = makeStyles((theme) => ({
-//   button: {
-//     display: "block",
-//     marginTop: theme.spacing(2),
-//   },
-//   formControl: {
-//     margin: theme.spacing(1),
-//     minWidth: 120,
-//   },
-// }));
-const useStyles = makeStyles((theme) => ({
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-}));
-const RegisterStudents = () => {
-  // events
-  const classes = useStyles();
 
-  // styling
-  const paperStyle = {
-    padding: "30px 20px",
-    width: 320,
-    height: "70vh",
-    margin: "20px auto",
-  };
+class RegisterStudents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      phoneNumber: '',
+      branch: '',
+      password: '',
+      year: '',
+      confirm: "",
+      role: "Student",
+      error: "",
+      success: ""
+    }
+    this.handleChange = (e) => {
+      return this.setState({ [e.target.name]: e.target.value })
+    };
+    this.handleSubmit = (e) => {
+      e.preventDefault()
+      const { name, email, phoneNumber, password, year, role } = this.state;
+      const data = { name, email, phoneNumber, password, year, role }
+      apiCallAuth('post', '/signup', data)
+        .then(async (response) => {
+          console.log(response)
+          await this.setState({ success: true })
+        }).catch((err) => {
+          return this.setState({ error: err })
+        });
+    }
+  }
 
-  const headerStyle = { margin: 0 };
-  const avatarStyle = { backgroundColor: "#1bbd7e" };
-  return (
-    <Grid>
-      <Paper elevation={20} style={paperStyle}>
-        <Grid align="center">
-          <Avatar style={avatarStyle}>
-            <AddCircleOutlineOutlinedIcon />
-          </Avatar>
-          <h2 style={headerStyle}>Sign Up</h2>
-          <Typography variant="caption" gutterBottom>
-            Please fill this form to create an account !
+
+  render() {
+    const headerStyle = { margin: 0 };
+    const avatarStyle = { backgroundColor: "#1bbd7e" };
+    const paperStyle = {
+      padding: "30px 20px",
+      width: 320,
+      height: "70vh",
+      margin: "20px auto",
+    };
+
+    return (
+      <Grid>
+        <Paper elevation={20} style={paperStyle}>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <AddCircleOutlineOutlinedIcon />
+            </Avatar>
+            <h2 style={headerStyle}>Sign Up</h2>
+            <Typography variant="caption" gutterBottom>
+              Please fill this form to create an account !
           </Typography>
-        </Grid>
-        <form>
-          <TextField fullWidth label="Name" placeholder="Enter your name" />
-          <TextField fullWidth label="Email" placeholder="Enter your email" />
+          </Grid>
+          <form onSubmit={this.handleSubmit}>
+            <TextField fullWidth label="Name" name="name" placeholder="Enter your name"
+              onChange={this.handleChange} required />
+            <TextField fullWidth label="Email" name="email" type="email" placeholder="Enter your email"
+              onChange={this.handleChange} required />
 
-          <TextField
-            fullWidth
-            label="Phone Number"
-            placeholder="Enter your phone number"
-          />
+            <TextField
+              fullWidth
+              label="Phone Number"
+              name="phoneNumber"
+              placeholder="Enter your phone number"
+              onChange={this.handleChange}
+              required
+            />
 
-          <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="grouped-select">Branch</InputLabel>
-            <Select defaultValue="" id="grouped-select">
-              <MenuItem value={"CS"}>CS</MenuItem>
-              <MenuItem value={"IT"}>IT</MenuItem>
-              <MenuItem value={"Extc"}>Extc</MenuItem>
-              <MenuItem value={"Mech"}>Mech</MenuItem>
-              <MenuItem value={"Etrx"}>Etrx</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl className={classes.formControl}>
-            <InputLabel htmlFor="grouped-select">Year</InputLabel>
-            <Select defaultValue="" id="grouped-select">
-              <MenuItem value={"FY"}>FY</MenuItem>
-              <MenuItem value={"SY"}>SY</MenuItem>
-              <MenuItem value={"TY"}>TY</MenuItem>
-              <MenuItem value={"LY"}>LY</MenuItem>
-            </Select>
-          </FormControl>
-          <TextField
-            fullWidth
-            label="Password"
-            placeholder="Enter your password"
-          />
-          <TextField
-            fullWidth
-            label="Confirm Password"
-            placeholder="Confirm your password"
-          />
-          <FormControlLabel
-            control={<Checkbox name="checkedA" />}
-            label="I accept the terms and conditions."
-          />
-          <Button type="submit" variant="contained" color="primary">
-            Sign up
-          </Button>
-        </form>
-      </Paper>
-    </Grid>
-  );
+            <FormControl style={{ margin: '3px', minWidth: '120px' }}>
+              <InputLabel htmlFor="grouped-select">Branch</InputLabel>
+              <Select defaultValue="" id="grouped-select"
+                onChange={this.handleChange} required name="branch">
+                <MenuItem value={"CS"}>CS</MenuItem>
+                <MenuItem value={"IT"}>IT</MenuItem>
+                <MenuItem value={"Extc"}>Extc</MenuItem>
+                <MenuItem value={"Mech"}>Mech</MenuItem>
+                <MenuItem value={"Etrx"}>Etrx</MenuItem>
+              </Select>
+            </FormControl>
+            <FormControl style={{ margin: '3px', minWidth: '150px' }}>
+              <InputLabel htmlFor="grouped-select">Year</InputLabel>
+              <Select defaultValue="" id="grouped-select"
+                onChange={this.handleChange} required name="year">
+                <MenuItem value={"FY"}>FY</MenuItem>
+                <MenuItem value={"SY"}>SY</MenuItem>
+                <MenuItem value={"TY"}>TY</MenuItem>
+                <MenuItem value={"LY"}>LY</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              name="password"
+              required
+              fullWidth
+              label="Password"
+              placeholder="Enter your password"
+              onChange={this.handleChange}
+              type="password"
+
+            />
+            <TextField
+              required
+              fullWidth
+              label="Confirm Password"
+              placeholder="Confirm your password"
+              onChange={this.handleChange}
+              name="confirm"
+              type="password"
+            />
+            <Button type="submit" variant="contained" color="primary" style={{ margin: '10px 0' }}>
+              Sign up
+            </Button>
+          </form>
+          {this.state.error !== "" &&
+            <Alert severity="error">{this.state.error}</Alert>
+          }
+          {this.state.success &&
+            <Alert severity="success">A verification link has been sent to your Email. Please Click on the link to verify your Email. </Alert>
+          }
+        </Paper>
+      </Grid >
+    );
+  }
 };
 
 export default RegisterStudents;
