4736bbf1512b547fa4822182cf1c04ad01669311
Vedant13-ux <56965523+Vedant13-ux@users.noreply.github.com>
2021-02-27 15:08:10 +0530

    backend

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ app.listen(process.env.PORT || 3001, process.env.IP, () => {
 const errorHandler = require('./handlers/errorHandler');
 require('dotenv').config();
 // Middleware
-const { loginRequired, ensureCorrectUser } = require('./middleware');
+// const { loginRequired, ensureCorrectUser } = require('./middleware');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,9 +31,7 @@ const userRoutes = require('./routes/user');
 
 // Incuding Routes
 app.use('/api/auth', authRoutes);
-app.use('/api/:secureId/community', loginRequired, ensureCorrectUser, communityRoutes);
-app.use('/api/:secureId/internship', loginRequired, ensureCorrectUser, internshipRoutes);
-app.use('/api/:secureId', loginRequired, ensureCorrectUser, userRoutes)
+app.use('/api', userRoutes)
 
 
 app.use((req, res, next) => {
