12f528113008872910e3408702171d9a5550e4c5
anvil <therajtiwari254@gmail.com>
2021-02-27 15:15:28 +0530

    routes added

--- front-end/src/components/Homepage.js
+++ front-end/src/components/Homepage.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react'
-import Navbar from '../containers/Global/Navbar'
+import React, { Component } from "react";
+import Navbar from "../containers/Global/Navbar";
 
-
-class Homepage extends Component{
-    render(){
-        return <div>
-            <Navbar></Navbar>
-        </div>
-    }
+class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar></Navbar>
+      </div>
+    );
+  }
 }
-export default Homepage
+export default Homepage;
